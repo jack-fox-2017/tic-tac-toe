67e42f6e9ddce000f62f749e9084d21486341b9b
Renataclara <renataclarak@gmail.com>
2017-07-27 21:42:26 +0700

    already checked just did it just because wanna correct it

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 function tic_tac_toe_board () {
-  var original = makeXO()
-  var result = shuffleXO(original);
+  var result = makeXO()
   var board = boardForm(result)
   return board;
 }
 
 function makeXO () {
-  var input = ['x','x','x','x','o','o','o','o']
-  var random = Math.floor(Math.random()*2)
-  if (random ==1) {
-    input.push('x');
-  } else {
-    input.push('o');
+  var input = []
+  var countX=0;
+  var countO=0;
+  while (countX <5 && countO<5) {
+    var random = Math.floor(Math.random()*2)
+    if (random ==1) {
+      input.push('x');
+      countX ++;
+      // console.log('xxxx');
+      } else {
+      input.push('o');
+      countO ++;
+      // console.log('ooooo');
+      }
   }
-  return input;
-}
 
-function shuffleXO(ori) {
-  for(i=0; i<ori.length; i++) {
-    var temp =0;
-    var j = Math.floor(Math.random()*(i+1))
-    temp = ori[i]
-    ori[i] = ori[j]
-    ori[j] = temp;
+  if (countO == 5){
+    // console.log('--count o : '+countO);
+    // console.log('--count x : '+countX);
+    while (input.length<9) {
+      input.push('x');
+      countX++;
+    }
   }
-  return ori;
+
+    if (countX == 5){
+      // console.log('--count o : '+countO);
+      // console.log('--count x : '+countX);
+      while (input.length<9) {
+        input.push('o');
+        countO++;
+      }
+    }
+                        // console.log('--count o : after '+countO);
+                        // console.log('--count x : after'+countX);
+  return input;
 }
 
 function boardForm(prefinal) {
@@ -40,4 +56,15 @@ function boardForm(prefinal) {
   return final;
 }
 
-console.log(tic_tac_toe_board())
+console.log(tic_tac_toe_board());
+
+// function shuffleXO(ori) {
+//   for(i=0; i<ori.length; i++) {
+//     var temp =0;
+//     var j = Math.floor(Math.random()*(i+1))
+//     temp = ori[i]
+//     ori[i] = ori[j]
+//     ori[j] = temp;
+//   }
+//   return ori;
+// }
